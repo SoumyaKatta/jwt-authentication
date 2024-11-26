ea340cc0d932296172fb157098e4785960736a4e
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-26 14:13:27 +0530

    Update server file by creating the route to get the posts

---
+++ server.ts
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const posts = [
+    {
+        username:"soumya",
+        title:"post 1"
+    },
+    {
+        username:"sakshi",
+        title:"post 2"
+    }
+]
+app.get('/posts',(req:any,res:any)=>{
+   res.json(posts)
+})
+
+app.listen(3000)
