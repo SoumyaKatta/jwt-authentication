ba947458472d91476185e8fed3c1af8d316c71a7
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-26 15:06:55 +0530

    Create a middleware for making authentication of user

--- server.ts
+++ server.ts
@@ -13,8 +13,8 @@ const posts = [
         title:"post 2"
     }
 ]
-app.get('/posts',(req:any,res:any)=>{
-   res.json(posts)
+app.get('/posts',authenticateToken,(req:any,res:any)=>{
+   res.json(posts.filter((post=>post.username === req.user.name)))
 })
 app.post('/login',(req:any,res:any)=>{
    const username = req.body.username
@@ -22,6 +22,17 @@ app.post('/login',(req:any,res:any)=>{
    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET )
    res.json({accessToken:accessToken})
 })
+function authenticateToken(req:any,res:any,next:any){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if(token == null) return res.sendStatus(401)
 
+ jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err:any,user:any)=>{
+    if(err) return res.sendStatus(403)
+        req.user = user
+       next()
+
+ })
+}
 
 app.listen(3000)
