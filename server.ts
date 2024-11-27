aa3c5a2eebbe344649d53d90ece53db5ce1d9751
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-27 12:44:05 +0530

    Remove the login route

--- server.ts
+++ server.ts
@@ -16,12 +16,12 @@ const posts = [
 app.get('/posts',authenticateToken,(req:any,res:any)=>{
    res.json(posts.filter((post=>post.username === req.user.name)))
 })
-app.post('/login',(req:any,res:any)=>{
-   const username = req.body.username
-   const user = {name:username}
-   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET )
-   res.json({accessToken:accessToken})
-})
+// app.post('/login',(req:any,res:any)=>{
+//    const username = req.body.username
+//    const user = {name:username}
+//    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET )
+//    res.json({accessToken:accessToken})
+// })
 function authenticateToken(req:any,res:any,next:any){
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
